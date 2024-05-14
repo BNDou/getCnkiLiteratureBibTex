787b5ae369fc23e0474e0de0eb74b52e9fbc39b6
BNDou <67632799+BNDou@users.noreply.github.com>
2024-05-15 05:12:45 +0800

    Update getCnkiLiteratureBibtex.js

--- getCnkiLiteratureBibtex.js
+++ getCnkiLiteratureBibtex.js
@@ -1,114 +1,69 @@
 // ==UserScript==
-// @name        知网_文献_bibtex提取
-// @description 从知网文献中直接复制bibtex
-// @namespace   getCnkiLiteratureBibtex
-// @version     1.3.0
-// @author      BN_Dou
-// @license     AGPL License
-// @match       https://kns.cnki.net/kcms/detail/detail.aspx?dbcode=*
-// @match       http://kns.cnki.net/kcms/detail/detail.aspx?dbcode=*
-// @require     https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js
-// @require     https://cdn.jsdelivr.net/gh/zh-lx/pinyin-pro@latest/dist/pinyin-pro.js
-// @grant       unsafeWindow
+// @name         知网-文献-bibtex提取
+// @description  从知网文献中直接复制bibtex
+// @author       BN_Dou
+// @version      2.1.0
+// @namespace    https://github.com/BNDou/getCnkiLiteratureBibTex
+// @match        https://greasyfork.org/zh-CN/users/883089-bndou
+// @match        https://kns.cnki.net/kcms2/article/abstract?v=*
+// @match        https://kns.cnki.net/kcms/detail/detail.aspx?dbcode=*
+// @match        http://kns.cnki.net/kcms/detail/detail.aspx?dbcode=*
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @icon         https://www.cnki.net/favicon.ico
+// @grant        none
+// @license      AGPL License
+// @downloadURL  https://update.greasyfork.org/scripts/444428/%E7%9F%A5%E7%BD%91-%E6%96%87%E7%8C%AE-bibtex%E6%8F%90%E5%8F%96.user.js
+// @updateURL    https://update.greasyfork.org/scripts/444428/%E7%9F%A5%E7%BD%91-%E6%96%87%E7%8C%AE-bibtex%E6%8F%90%E5%8F%96.meta.js
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
 
-    var jQuery = unsafeWindow.jQuery;
-    var { pinyin } = pinyinPro;
+    //添加按钮
+    var otherButtons = document.getElementsByClassName('other-btns')[0];
+    var new_ul = document.createElement('ul');
+    var button = document.createElement('button');
+    button.id = "bibbtn";
+    button.title = "BibTex"
+    // 设置其他按钮的属性和样式
+    button.innerHTML = "⭕BibTex";
+    button.style = "width: 80px; height: 25px; border-radius: 8px; background-color: #4CAF50; border: none; color: white; font-size: 16px; font-weight: bold; text-align: center; cursor: pointer; float: right;";
+    button.onmouseover = function() { this.style.backgroundColor = '#3e8e41'; };
+    button.onmouseout = function () { this.style.backgroundColor = '#4CAF50';};
+    new_ul.appendChild(button);
+    otherButtons.appendChild(new_ul);
 
-    jQuery(document).ready(function ($) {
-
-        window.onload = function () {
-
-            var a = document.getElementById("paramdbname")
-            var b = document.getElementById("paramfilename")
-            var fileid = a.getAttribute("value") + '!' + b.getAttribute("value") + '!1!0'
-
-            //添加按钮
-            var x = document.getElementsByClassName("btn-tool")
-            var input = document.createElement('li')
-            input.setAttribute("id", "bibbtn")
-            input.setAttribute("class", "btn-note")
-            input.setAttribute("title", "BibTex")
-            input.innerHTML = ">>BibTex<<"
-            input.style = "width: 72px;height: 23px;cursor: pointer;color: #e8e6e3;"
-            x[0].children[0].append(input)
-
-            //按下按钮
-            $("#bibbtn").click(function () {
-                $.post("https://kns.cnki.net/kns8/manage/APIGetExport",
-                {
-                    filename: fileid,
-                    displaymode: "NoteExpress"
-                },
-
-                function (data) {
-                    console.log(data);
-                    var bibtext = ""
-                    var ss = data.data[0].value[0]
-                    var ssl = ss.split("<br>")
-                    var label = ""
-                    for (var j = 0; j < ssl.length - 1; j++) {
-                        var kk = ssl[j].toLocaleLowerCase().split(" ").join("").split("}:")
-                        if (kk[0] == "{author") {
-                            label = kk[1].split(";")[0]
-                        }
-                    }
-                    for (var i = 0; i < ssl.length - 1; i++) {
-                        var k = ssl[i].toLocaleLowerCase().split(" ").join("").split("}:")
-                        var item = k[0]
-                        var detail = k[1]
-                        if (item == "{referencetype") {
-                            if (detail == "journalarticle") {
-                                bibtext = "@article{" + pinyin(label, { toneType: 'none' }).split(" ").join("") + ",\n"
-                            }
-                            else if (detail == "conferenceproceedings") {
-                                bibtext = "@inproceedings{" + pinyin(label, { toneType: 'none' }).split(" ").join("") + ",\n"
-                            }
-                            else if (detail == "patent") {
-                                bibtext = "@patent{" + pinyin(label, { toneType: 'none' }).split(" ").join("") + ",\n"
-                            }
-                            else if (detail == "thesis") {
-                                bibtext = "@thesis{" + pinyin(label, { toneType: 'none' }).split(" ").join("") + ",\n"
-                            }
-                        }
-                        else if (item == "{issue") {
-                            bibtext = bibtext + "  number={" + detail + "},\n"
-                        }
-                        else if (item == "{title") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.toLocaleUpperCase() + "},\n"
-                        }
-                        else if (item == "{author") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.split(";").join(" and ").substr(0, detail.split(";").join(" and ").length - 5) + "},\n"
-                        }
-                        else if (item == "{authoraddress") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.toLocaleUpperCase() + "},\n"
-                        }
-                        else if (item == "{journal") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.toLocaleUpperCase() + "},\n"
-                        }
-                        else if (item == "{keywords") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.toLocaleUpperCase() + "},\n"
-                        }
-                        else if (item == "{abstract") {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail.toLocaleUpperCase() + "},\n"
-                        }
-                        else {
-                            bibtext = bibtext + "   " + item.substr(1, item.length - 1) + "={" + detail + "},\n"
-                        }
-                    }
-                    bibtext += "}"
-                    //console.log(bibtext)
-                    const copad = document.createElement('textarea')
-                    copad.value = bibtext
-                    document.body.appendChild(copad)
-                    copad.select()
-                    document.execCommand('Copy')
-                    document.body.removeChild(copad)
-                });
-            })
+    var bibtex;
+    var fileid = document.getElementById("paramdbname").getAttribute("value") + '!' + document.getElementById("paramfilename").getAttribute("value") +'!1!0';
+    //按下按钮
+    $("#bibbtn").click(function(){
+        // 定义请求参数
+        const params = {
+            FileName: fileid,
+            DisplayMode: 'BibTex',
+            OrderParam: 0,
+            OrderType: 'desc',
         };
-    })
+
+        // 发送 POST 请求
+        fetch('https://kns.cnki.net/dm/api/ShowExport', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Referer': 'https://kns.cnki.net/dm/manage/export.html',
+            },
+            body: new URLSearchParams(params),
+        })
+            .then(response => response.text())
+            .then(data => {
+            // 将响应内容存储到 bibtex 变量中
+            bibtex = data.match(/<li>(.*?)<\/li>/s)[1].replace(/<br>/g, '');
+        }).catch(error => console.error(error));
+
+        if (bibtex) {
+            console.log(bibtex);
+            // 将内容复制到粘贴板
+            navigator.clipboard.writeText(bibtex);
+        };
+    });
 })();
